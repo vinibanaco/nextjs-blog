f2233876ec11539bf962867c0f3878a37a6b2d55
Vinícius Marrara Banaco <vinibanaco@gmail.com>
2020-10-14 15:17:31 -0300

    feat: follow the Next.js tutorial until SSR lesson

---
+++ components/alert.js
@@ -0,0 +1,16 @@
+import cn from 'classnames';
+
+import styles from './alert.module.scss';
+
+export default function Alert({ children, type }) {
+  return (
+    <div
+      className={cn({
+        [styles.success]: type === 'success',
+        [styles.error]: type === 'error',
+      })}
+    >
+      {children}
+    </div>
+  );
+}
