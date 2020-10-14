8ade11b35f2cdf8ffac546fbcffa57f0ef641167
Vinícius Marrara Banaco <vinibanaco@gmail.com>
2020-10-14 15:55:10 -0300

    feat: add the content and styles to the post page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,15 +1,27 @@
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
+import Head from 'next/head';
 import Layout from '../../components/layout';
+import Date from '../../components/date';
+
+import utilStyles from '../../styles/utils.module.scss';
 
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
@@ -23,7 +35,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
